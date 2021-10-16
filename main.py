24fb996ac683b37937c21b1de994475b9aea4ca9
Matthias Roussel <matthias.roussel01@gmail.com>
2021-10-16 21:17:41 +0200

    Improved stability in case of missing information in the json source file.

--- main.py
+++ main.py
@@ -40,21 +40,51 @@ class Resume:
                         services[account["service"]][1] + account["username"]
                     )
 
+            experiences = []
+            for experience in self.data["experience"]:
+                keywords = []
+                if "keywords" in experience:
+                    for keyword in experience["keywords"]:
+                        if isinstance(keyword, str):
+                            keyword = {"name": keyword}
+                        keywords.append(keyword)
+                experience["keywords"] = keywords
+                experiences.append(experience)
+            self.data["experience"] = experiences
+
             for acc in self.data["accomplishements"]:
                 acc["description"] = re.sub(r"\n", "<br/>", acc["description"])
+                github = None
+                for link in acc["links"]:
+                    if link["service"] == "github":
+                        github = link
+                acc["github"] = github
+                keywords = []
+                if "keywords" in acc:
+                    for keyword in acc["keywords"]:
+                        if isinstance(keyword, str):
+                            keyword = {"name": keyword}
+                        keywords.append(keyword)
+                acc["keywords"] = keywords
 
+
+            skills = []
             for skill in self.data["skills"]:
-                skill["score"] = skill["score"] if "score" in skill else 0
+                if isinstance(skill, str):
+                    skill = {"name": skill}
+                skill["score"] = skill["score"] if "score" in skill else 50
+                skills.append(skill)
 
             proficient_skills = [
-                skill for skill in self.data["skills"] if skill["score"] >= 70
+                skill for skill in skills if skill["score"] >= 70
             ]
             non_proficient_skills = [
                 skill
-                for skill in self.data["skills"]
-                if skill["score"] < 70
+                for skill in skills
+                if (skill["score"] < 70 and skill["score"] >= 10)
             ]
-            self.data["skills"] = proficient_skills + non_proficient_skills
+            weak_skills = [skill for skill in skills if skill["score"] < 10]
+            self.data["skills"] = proficient_skills + non_proficient_skills + weak_skills
 
     def render(self, template_name):
         template = env.get_template(template_name + ".html")
