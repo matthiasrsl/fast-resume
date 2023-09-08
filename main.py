5804e28b9c2ff9913f27f7090752aa8bd2bbfc09
Matthias Roussel <matthias.roussel01@gmail.com>
2023-09-08 19:18:59 +0200

    feat: Added argument parser.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import sys
+import argparse
 
 from src.resume import Resume
 
 if __name__ == "__main__":
-    resume = Resume(sys.argv[1])
+    argparser = argparse.ArgumentParser("Fast resume : Quickly generate beautiful resumes from JSON.")
+    argparser.add_argument("--resume", help="Name of the JSON file containing the resume data, without the extension.")
+    argparser.add_argument("--template", help="Name of the template to use.", default="zeng")
+    args = argparser.parse_args()
+
+    resume = Resume(args.resume)
     resume.preprocess()
     print(resume)  # noqa: T201
-    # resume.render(sys.argv[2])
+    resume.render(args.template)
